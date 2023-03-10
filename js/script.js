2beb6e34276444c1b2f690f2157010f6486e7c7a
banover <qkrwotn5713@naver.com>
2023-03-10 23:59:30 +0900

    sticky nav 구현

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// sticky navigation
+
+const sectionHomeEl = document.querySelector(".section-home");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    console.log(entries);
+    const entry = entries[0];
+    console.log(entry);
+
+    if (entry.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (entry.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-120px",
+  }
+);
+
+observer.observe(sectionHomeEl);
+
+// smooth scrolling
+
+const navLinks = document.querySelectorAll(".header a");
+console.log(navLinks);
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    if (href !== "#") {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
